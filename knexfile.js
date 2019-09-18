446ee6da8fc352e3c3227e0d7c180f8d800aa3ed
Aaron T <troskaaaron@gmail.com>
2019-09-18 18:50:02 -0500

    created knexfile

---
+++ knexfile.js
@@ -0,0 +1,37 @@
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: { filename: './database/auth.db3' },
+    useNullAsDefault: true, // needed for sqlite
+    migrations: {
+      directory: './database/migrations',
+      tableName: 'knex_migrations',
+    },
+    seeds: { directory: './database/seeds'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done)
+      }
+    }
+  },
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/test.db3',
+    },
+    useNullAsDefault: true, // needed for sqlite
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done)
+      }
+    }
+  }
+};
